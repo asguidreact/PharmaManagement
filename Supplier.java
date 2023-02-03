e6ef2411e0aa9558390290b8850de127c2ccf978
asguidreact <117361714+asguidreact@users.noreply.github.com>
2023-02-03 19:04:17 +0530

    Supplier model

---
+++ Supplier.java
@@ -0,0 +1,38 @@
+package com.management.Pharma.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Supplier_table")
+public class Supplier {
+	
+	@Id
+	@Column(name="supplier_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int supplierId;
+	private String supplierName;
+	
+	
+	public int getSupplierId() {
+		return supplierId;
+	}
+	public void setSupplierId(int supplierId) {
+		this.supplierId = supplierId;
+	}
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	
+	
+	
+	
+	
+}
